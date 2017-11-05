a5f7371b8ca293186f86f4e6b807b6a2155060d2
Phuc <phuc.nguyen123456@gmail.com>
2017-11-05 08:57:02 -0500

    init

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ router.get('/signup', function(req, res, next){
     res.render('register.html')
 });
 
+router.get('/sign/:info', function(req, res, next){
+    console.log(req.params.info)
+    res.send('ok')
+});
+router.get('/tinder', function(req, res, next){
+   res.render('tinder.html')
+});
+
 
 
 module.exports = router
